9f8e9fd55bc3a86b2644e1acc11a983c30623e2d
fatemeh <fatemeh.developer@gmail.com>
2023-09-18 09:30:37 +0330

    - Implement AddSortsQuery func helper - Implement AddPaginationQuery  func helper - Implement ConvertSqlQuery  func helper - Implement statelessParser for filter

---
+++ helper.go
@@ -0,0 +1,179 @@
+package postgresql
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/mis-hashemi/pgcontroller/page"
+	"github.com/mis-hashemi/pgcontroller/sort"
+	"github.com/mis-hashemi/request-parameter/query"
+)
+
+func AddSortsQuery(query string, sorts []sort.Sort) string {
+	if len(sorts) == 0 {
+		return query
+	}
+
+	sortQuery := " ORDER BY"
+	for i, sort := range sorts {
+		if i > 0 {
+			sortQuery += ","
+		}
+		sortQuery += " " + sort.GetName()
+		if !sort.IsAscending() {
+			sortQuery += " DESC"
+		}
+	}
+
+	return query + sortQuery
+}
+
+func AddPaginationQuery(query string, page page.Pagination) string {
+	if page == nil {
+		return query
+	}
+
+	limit := page.GetLimit()
+	skip := page.GetSkip()
+
+	if limit > 0 {
+		query += " LIMIT " + strconv.FormatUint(uint64(limit), 10)
+	}
+
+	if skip > 0 {
+		query += " OFFSET " + strconv.FormatUint(uint64(skip), 10)
+	}
+
+	return query
+}
+
+const (
+	SqlIsNull    = "IS NULL"
+	SqlIsNotNull = "IS NOT NULL"
+)
+
+type SqlOperator string
+
+const (
+	SqlOperatorEqual      SqlOperator = "="
+	SqlOperatorNotEqual   SqlOperator = "!="
+	SqlEqualOrMoreThan    SqlOperator = ">="
+	SqlMoreThan           SqlOperator = ">"
+	SqlLessThan           SqlOperator = "<"
+	SqlLessOrEqualThan    SqlOperator = "<="
+	SqlOperatorContain    SqlOperator = "LIKE"
+	SqlOperatorNotContain SqlOperator = "NOT LIKE"
+	SqlOperatorIn         SqlOperator = "IN"
+	SqlOperatorNotIn      SqlOperator = "NOT IN"
+	SqlOperatorEmpty      SqlOperator = "LIKE ''"
+	SqlOperatorNotEmpty   SqlOperator = "NOT LIKE ''"
+)
+const (
+	SqlParameter         = "(?)"
+	SqlParameterMultiple = "(?)"
+)
+
+type ParameterExpectation int
+
+const (
+	ParameterExpectationZero ParameterExpectation = iota
+	ParameterExpectationSingle
+	ParameterExpectationMultiple
+)
+
+func getOperatorInfo(op query.QueryOperator) (SqlOperator, ParameterExpectation) {
+
+	switch op {
+	case query.QueryOperatorEqual:
+		return SqlOperatorEqual, ParameterExpectationSingle
+	case query.QueryOperatorNotEqual:
+		return SqlOperatorNotEqual, ParameterExpectationSingle
+	case query.QueryOperatorMoreThan:
+		return SqlMoreThan, ParameterExpectationSingle
+	case query.QueryOperatorEqualOrMoreThan:
+		return SqlEqualOrMoreThan, ParameterExpectationSingle
+	case query.QueryOperatorLessThan:
+		return SqlLessThan, ParameterExpectationSingle
+	case query.QueryOperatorEqualOrLessThan:
+		return SqlLessOrEqualThan, ParameterExpectationSingle
+	case query.QueryOperatorContain:
+		return SqlOperatorContain, ParameterExpectationSingle
+	case query.QueryOperatorNotContain:
+		return SqlOperatorNotContain, ParameterExpectationSingle
+	case query.QueryOperatorEmpty:
+		return SqlOperatorEmpty, ParameterExpectationZero
+	case query.QueryOperatorNotEmpty:
+		return SqlOperatorNotEmpty, ParameterExpectationZero
+	case query.QueryOperatorIn:
+		return SqlOperatorIn, ParameterExpectationMultiple
+	case query.QueryOperatorNotIn:
+		return SqlOperatorNotIn, ParameterExpectationMultiple
+	}
+
+	panic("unknown Operator")
+}
+func ConvertSqlQuery(queryInfo query.QueryInfo, tblAlias string, placeholderCounter *int) (string, []interface{}) {
+	whereClause, values := getWhereClause(queryInfo, tblAlias, placeholderCounter)
+
+	// Append the WHERE clause to the original query
+	// sqlQuery := fmt.Sprintf("%s WHERE %s", query, whereClause)
+
+	return whereClause, values
+}
+
+func getWhereClause(queryInfo query.QueryInfo, tblAlias string, placeholderCounter *int) (string, []interface{}) {
+	logicOperator := "AND"
+	if !queryInfo.IsAnd() {
+		logicOperator = "OR"
+	}
+
+	queries := queryInfo.GetQuery()
+	clauses := make([]string, len(queries))
+	var clauseValues []interface{}
+
+	for i, q := range queries {
+		clause, values := buildClause(q, tblAlias, placeholderCounter)
+		clauses[i] = clause
+		if values != nil {
+			clauseValues = append(clauseValues, values...)
+		}
+	}
+
+	whereClause := strings.Join(clauses, " "+logicOperator+" ")
+	return whereClause, clauseValues
+}
+
+func buildClause(q query.Query, tblAlias string, placeholderCounter *int) (string, []interface{}) {
+	fieldName := q.GetName()
+	if tblAlias != "" {
+		fieldName = tblAlias + "." + fieldName
+	}
+	operator, _ := getOperatorInfo(q.GetOperator())
+	operand := q.GetOperand()
+
+	switch operator {
+	case SqlOperatorIn, SqlOperatorNotIn:
+		values := strings.Split(operand.Value.(string), ",")
+		placeholders := make([]string, len(values))
+		clauseValues := make([]interface{}, len(values))
+		for i := range values {
+			*placeholderCounter++
+			placeholders[i] = fmt.Sprintf("$%d", *placeholderCounter)
+			clauseValues[i] = values[i]
+		}
+		clause := fmt.Sprintf("%s %s (%s)", fieldName, operator, strings.Join(placeholders, ", "))
+		return clause, clauseValues
+	case SqlOperatorEmpty, SqlOperatorNotEmpty:
+		clause := fmt.Sprintf("%s %s ", fieldName, operator)
+		return clause, nil
+	case SqlOperatorContain, SqlOperatorNotContain:
+		*placeholderCounter++
+		clause := fmt.Sprintf("%s %s '%%' || $%d || '%%'", fieldName, operator, *placeholderCounter)
+		return clause, []interface{}{operand.Value}
+	default:
+		*placeholderCounter++
+		clause := fmt.Sprintf("%s %s $%d", fieldName, operator, *placeholderCounter)
+		return clause, []interface{}{operand.Value}
+	}
+}
---
+++ parser.go
@@ -0,0 +1,182 @@
+package postgresql
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/mis-hashemi/pgcontroller/filter"
+	"github.com/mis-hashemi/request-parameter/query"
+)
+
+type statelessParser struct{}
+
+func NewParser() statelessParser {
+	return statelessParser{}
+}
+
+func (statelessParser) ParseFilter(f filter.Filter) (string, error) {
+	if f == nil {
+		return "", nil
+	}
+	applier := &applyer{}
+	sqlQuery, err := f.Execute(applier)
+	if err != nil {
+		return "", err
+	}
+	return sqlQuery.(string), nil
+}
+
+func getJsonOperatorInfo(op query.QueryOperator) (string, error) {
+
+	switch op {
+	case query.QueryOperatorEqual:
+		return string(SqlOperatorEqual), nil
+	case query.QueryOperatorNotEqual:
+		return "<>", nil
+	case query.QueryOperatorContain:
+		return string(SqlOperatorContain), nil
+	case query.QueryOperatorNotContain:
+		return string(SqlOperatorNotContain), nil
+	case query.QueryOperatorEmpty:
+		return SqlIsNull, nil
+	case query.QueryOperatorNotEmpty:
+		return SqlIsNotNull, nil
+	case query.QueryOperatorMoreThan:
+		return string(SqlMoreThan), nil
+	case query.QueryOperatorEqualOrMoreThan:
+		return string(SqlEqualOrMoreThan), nil
+	case query.QueryOperatorLessThan:
+		return string(SqlLessThan), nil
+	case query.QueryOperatorEqualOrLessThan:
+		return string(SqlLessOrEqualThan), nil
+	case query.QueryOperatorIn:
+		return string(SqlOperatorIn), nil
+	case query.QueryOperatorNotIn:
+		return string(SqlOperatorIn), nil
+	}
+
+	return "", fmt.Errorf("Operator %s not supported", op)
+}
+
+type applyer struct {
+	Vars      []any
+	LastQuery filter.FilterQuery
+}
+
+func (a *applyer) Execute(filterQuery filter.FilterQuery) (any, error) {
+	operand := filterQuery.GetOperand()
+	op, err := getJsonOperatorInfo(filterQuery.GetOperator())
+	if err != nil {
+		return nil, err
+	}
+	if subFilter, ok := operand.Value.(filter.Filter); ok {
+		subQuery, err := subFilter.Execute(a)
+		if err != nil {
+			return nil, err
+		}
+		return a.buildCondition(filterQuery.GetName(), op, subQuery), nil
+	}
+	if operand != nil && operand.Value != nil {
+		return a.buildCondition(filterQuery.GetName(), op, operand.Value), nil
+	}
+
+	switch op {
+	case SqlIsNull:
+		return fmt.Sprintf("( %s IS NULL)", filterQuery.GetName()), nil
+	case SqlIsNotNull:
+		return fmt.Sprintf("(%s IS NOT NULL)", filterQuery.GetName()), nil
+	default:
+		return nil, fmt.Errorf("operator %s not supported for nil operand", op)
+	}
+}
+
+func (a *applyer) buildCondition(fieldName, operator string, value interface{}) string {
+	if operator == string(SqlOperatorIn) || operator == string(SqlOperatorNotIn) {
+		valuesAny := value.([]any)
+		values := make([]string, len(valuesAny))
+		for i, v := range valuesAny {
+			values[i] = fmt.Sprintf("'%s'", v)
+		}
+		return fmt.Sprintf("%s %s (%s)", fieldName, operator, strings.Join(values, ","))
+	}
+	if operator == string(SqlOperatorContain) {
+		return fmt.Sprintf("%s %s '%%%s%%'", fieldName, operator, value)
+	}
+	var castType string
+	switch v := value.(type) {
+	case string:
+		castType = "text"
+	case int, int32, int64, float64:
+		castType = "int"
+	case bool:
+		castType = "boolean"
+	case time.Time:
+		castType = "timestamp"
+	// case float64:
+	// 	castType = "double precision"
+	default:
+		fmt.Printf("value type %T!\n", v)
+	}
+	if castType != "" {
+		return fmt.Sprintf("(%s)::%s %s %v", fieldName, castType, operator, value)
+	}
+
+	return fmt.Sprintf("%s %s %v", fieldName, operator, value)
+}
+
+func (a *applyer) And(left, right filter.Filter) (any, error) {
+	leftEx, err := left.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	rightEx, err := right.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	return " " + leftEx.(string) + " AND " + rightEx.(string), nil
+}
+
+func (a *applyer) Not(wrapped filter.Filter) (any, error) {
+	wrappedEx, err := wrapped.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	return " NOT (" + wrappedEx.(string) + ")", nil
+}
+
+func (a *applyer) Or(left, right filter.Filter) (any, error) {
+	leftEx, err := left.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	rightEx, err := right.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	return " " + leftEx.(string) + " OR " + rightEx.(string), nil
+}
+
+func (a *applyer) GroupAnd(left, right filter.Filter) (any, error) {
+	leftEx, err := left.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	rightEx, err := right.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	return " " + leftEx.(string) + " AND (" + rightEx.(string) + ")", nil
+}
+
+func (a *applyer) GroupOr(left, right filter.Filter) (any, error) {
+	leftEx, err := left.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	rightEx, err := right.Execute(a)
+	if err != nil {
+		return nil, err
+	}
+	return " " + leftEx.(string) + " OR (" + rightEx.(string) + ")", nil
+}
